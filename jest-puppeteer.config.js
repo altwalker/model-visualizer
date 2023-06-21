7882aa84f2d34ca822df4c7b4c0662f102e3c1a1
Robert-96 <dezmereanrobert@gmail.com>
2023-06-22 02:51:09 +0300

    Fix jest config

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   server: {
-    command: 'npx webpack-dev-server --content-base examples/ --host 0.0.0.0 --port 8081',
+    command: 'npx webpack-dev-server --host 0.0.0.0 --port 8081',
     port: 8081,
     launchTimeout: 10000
   },
